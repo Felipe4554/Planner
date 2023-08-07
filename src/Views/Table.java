8208b591539957b0553d025463a750f201a7c200
Felipe Chacon Vargas <felichaconv@gmail.com>
2023-08-07 15:19:48 -0600

    Interface Table
    Se arregla el error de paquete

--- src/Views/Table.java
+++ src/Views/Table.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package Codigo;
+package Views;
 
 import javax.swing.JTable;
 import javax.swing.RowFilter;
